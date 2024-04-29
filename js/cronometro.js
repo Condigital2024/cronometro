fdcbb8fffeaef56078b78c1a089e884063f4cdf8
Marlly <contenidodigital@marketmedios.com.co>
2024-04-29 11:50:57 -0500

    letf

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,7 +1,7 @@
 function countdown() {
     const now = new Date();
     const tomorrow = new Date();
-    tomorrow.setHours(16, 58, 0, 0);
+    tomorrow.setHours(10, 16, 0, 0);
     if (now >= tomorrow){
         tomorrow.setDate(tomorrow.getDate() + 1);
     }
